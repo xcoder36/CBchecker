3cbc20dac685bb84054524e763d2677386d8b96f
david <david.lambert@ynov.com>
2017-03-19 15:38:01 +0100

    delete composer

--- tests/codeceptionacceptancetest.php
+++ tests/codeceptionacceptancetest.php
@@ -1,13 +1,22 @@
 <?php
-$I = new AcceptanceTester($scenario);
-$I->am('user');
-$I->wantTo('login to website');
-$I->lookForwardTo('access all website features');
-$I->amOnPage('../index.php');
-$I->fillField('number','davert');
-$I->fillField('CVV','qwerty');
-$I->fillField('Month','qwerty');
-$I->fillField('Year','qwerty');
-$I->fillField('price','qwerty');
-$I->click('OK');
-$I->see('resultat :');
+
+
+class BasicCest
+{
+    // test
+    public function checkLogin(\AcceptanceTester $I)
+    {
+        $I = new AcceptanceTester($scenario);
+        $I->am('user');
+        $I->wantTo('login to website');
+        $I->lookForwardTo('access all website features');
+        $I->amOnPage('../index.php');
+        $I->fillField('number','davert');
+        $I->fillField('CVV','qwerty');
+        $I->fillField('Month','qwerty');
+        $I->fillField('Year','qwerty');
+        $I->fillField('price','qwerty');
+        $I->click('OK');
+        $I->see('resultat :');
+    }
+}
