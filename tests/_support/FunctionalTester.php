4c058bb125ac581445170a9234cb262cad1e5c29
david <david.lambert@ynov.com>
2017-03-19 16:11:21 +0100

    codeception build

--- tests/_support/FunctionalTester.php
+++ tests/_support/FunctionalTester.php
@@ -20,19 +20,8 @@ class FunctionalTester extends \Codeception\Actor
 {
     use _generated\FunctionalTesterActions;
 
-    public function checkLogin(\AcceptanceTester $I)
-    {
-        $I = new AcceptanceTester($scenario);
-        $I->am('user');
-        $I->wantTo('login to website');
-        $I->lookForwardTo('access all website features');
-        $I->amOnPage('../index.php');
-        $I->fillField('number','1234567890123456');
-        $I->fillField('CVV','123');
-        $I->fillField('Month','05');
-        $I->fillField('Year','2028');
-        $I->fillField('price','0');
-        $I->click('OK');
-        $I->see('resultat :');
-    }
+
+    /**
+     * Define custom actions here
+     */
 }
